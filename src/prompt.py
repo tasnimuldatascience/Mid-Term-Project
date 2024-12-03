4f96c54c7325b7511dd02cabe1242cb461648d4a
tasnimuldatascience <160681120+tasnimuldatascience@users.noreply.github.com>
2024-12-03 00:07:54 -0500

    Add files via upload

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If asked who is the creator of this project, simply reply Tasnimul Hasan who is a graduate student at University of Toledo. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
