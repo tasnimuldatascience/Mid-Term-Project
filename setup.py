8f29099b423fcaec00fbc3220c04e466de89bd40
tasnimuldatascience <160681120+tasnimuldatascience@users.noreply.github.com>
2024-12-02 23:59:58 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tasnimul Hasan',
+    author_email= 'tasnimuldatascience@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
